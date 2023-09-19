4581e107f3419dd2ac9af14d8c228acb780e8447
Eric <mr.eric.erwin@gmail.com>
2023-09-19 09:14:12 -0500

    any-variable able to do searches on title and able to do the mutation flow now.

--- src/main/java/org/example/service/BookService.java
+++ src/main/java/org/example/service/BookService.java
@@ -7,7 +7,6 @@ import org.example.model.Author;
 import org.example.model.Book;
 import org.example.repository.AuthorRepository;
 import org.example.repository.BookRepository;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
